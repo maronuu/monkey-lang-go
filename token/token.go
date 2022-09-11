32885ff3da2485c6e279ce3447260b793676b4f7
maronuu <oguri.ytr.2000@gmail.com>
2022-09-11 15:34:10 +0900

    Support 1 char token, keyword token

--- token/token.go
+++ token/token.go
@@ -2,8 +2,13 @@ package token
 
 type TokenType string
 
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
 const (
-	ILLEGAL = "Illegal"
+	ILLEGAL = "ILLEGAL"
 	EOF = "EOF"
 	// Identifier + Literals
 	IDENT = "IDENT"
@@ -23,3 +28,14 @@ const (
 	LET = "LET"
 )
 
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookUpIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
