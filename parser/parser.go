9bc48a880584c727d3aa9855dc0ddbd4e8286c7d
maronuu <oguri.ytr.2000@gmail.com>
2022-09-24 18:31:41 +0900

    Support identifiers

--- parser/parser.go
+++ parser/parser.go
@@ -8,22 +8,52 @@ import (
 	"github.com/maronuu/monkey-lang-go/token"
 )
 
+const (
+	_ int = iota
+	LOWEST
+	EQUALS // ==
+	LESSGREATER // <, >
+	SUM // +
+	PRODUCT // *
+	PREFIX // -X or !X
+	CALL // myFunc(X)
+)
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
 	l *lexer.Lexer
 	errors []string
 
 	curToken token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns map[token.TokenType]infixParseFn
+}
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l, errors: []string{}}
+	
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	// read 2 tokens
 	p.nextToken()
 	p.nextToken()
 
 	return p
 }
+
+
 
 func (p *Parser) Errors() []string {
 	return p.errors
@@ -62,7 +92,8 @@ func (p *Parser) parseStatement() ast.Statement {
 		// return statement
 		return p.parseReturnStatement()
 	default:
-		return nil
+		// otherwise, expression stmt
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -100,6 +131,32 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression() ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
